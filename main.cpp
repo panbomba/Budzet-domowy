285dd7162ed178c4e81ce8ac43d2656ed2a5d539
Maciej Bombik <maciej.bombik.programista@gmail.com>
2019-10-17 16:11:55 +0200

    main file created

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
-#include <ctime> //already includes struct tm
 
 using namespace std;
 
 int main()
 {
-    time_t teraz = time(0);
-    tm *ltm = localtime(&teraz);
-    int rok = 0, miesiac = 0, dzien = 0, dzienRoku = 0;
-
-    rok = (1900 + ltm->tm_year);
-    miesiac = (1 + ltm->tm_mon);
-    dzien = (ltm->tm_mday);
-    dzienRoku = (ltm->tm_yday);
-
-        // MAM DZIEKI TEMU ROK, MIESIAC I DZIEN W FORMIE INT - BEDZIE MOZNA WYKONYWAC DZIALANIA MATEMATYCZNE
-    cout << "Rok: " << rok <<endl;
-    cout << "Miesiac: "<< miesiac << endl;
-    cout << "Dzien: " << dzien << endl;
-    cout << "Jest to dzien roku numer: " << dzienRoku << endl;
-
+    cout << "Hello world!" << endl;
     return 0;
 }
